a9d1ec1c6f232bfb60d48300de669bef83d99a24
Irish Micole Cando <irishmicolecando@gmail.com>
2021-11-13 19:36:05 +0800

    Added math module and floor function to round up inputted grades

--- PUP-Grading-System.py
+++ PUP-Grading-System.py
@@ -5,6 +5,7 @@
 # Input grade: 87.6
 # Grade/Mark: 1.75
 # Description: Very Good
+import math
 
 def ask_user_name():
     user_ = input("Enter name: ")
@@ -12,20 +13,21 @@ def ask_user_name():
     return user_
 
 def grade_percentage():
-    input_grade = input("Input Grade: ")
-    if input_grade > 96 and input_grade <= 100:
+    input_grade = float(input("Input Grade: "))
+    round_up = math.floor(input_grade)
+    if round_up >= 97 and round_up <= 100:
         print(f"Grade/Mark: 1.0 \nDescription: Excellent")
-    elif input_grade > 93 and input_grade <= 96:
+    elif round_up >= 94 and round_up <= 96:
         print(f"Grade/Mark: 1.25 \nDescription: Excellent")
-    elif input_grade > 90 and input_grade <= 93:
+    elif round_up >= 91 and round_up <= 93:
         print(f"Grade/Mark: 1.50 \nDescription: Very Good")
-    elif input_grade > 87 and input_grade <= 90:
+    elif round_up >= 88 and round_up <= 90:
         print(f"Grade/Mark: 1.75 \nDescription: Very Good")
-    elif input_grade > 84 and input_grade <= 87:
+    elif round_up >= 85 and round_up <= 87:
         print(f"Grade/Mark: 2.0 \nDescription: Very Good")
-    elif input_grade > 81 and input_grade <= 84:
+    elif round_up >= 82 and round_up <= 84:
         print(f"Grade/Mark: 2.25 \nDescription: Very Good")
-    elif input_grade > 78 and input_grade <= 81:
+    elif round_up >= 79 and round_up <= 81:
         print(f"Grade/Mark: 2.25 \nDescription: Very Good")
 
     return input_grade
