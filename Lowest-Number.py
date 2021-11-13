217bcd0f31b117faaf7887171f241178e7cfec63
Irish Micole Cando <irishmicolecando@gmail.com>
2021-11-13 22:45:53 +0800

    Added variable to ask the user to input the first, second, and third numbers

---
+++ Lowest-Number.py
@@ -0,0 +1,8 @@
+# Program 2: Find the lowest number
+# Create a program that ask 3 numbers. 
+# Find the lowest number using only if-else statement.
+# Display the lowest number.
+
+number1 = float(input("Enter first number: "))
+number2 = float(input("Enter second number: "))
+number3 = float(input("Enter third number: "))
