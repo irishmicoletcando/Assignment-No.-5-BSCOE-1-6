d00c8ec1677bb4e1772d9d437e393a11e796def3
Irish Micole Cando <irishmicolecando@gmail.com>
2021-11-13 22:47:41 +0800

    Defined a function and added if condition for number 1

--- Lowest-Number.py
+++ Lowest-Number.py
@@ -3,6 +3,14 @@
 # Find the lowest number using only if-else statement.
 # Display the lowest number.
 
+def ask_num(num1, num2, num3):
+    if num1 < num2 and num1 < num3:
+        print(f"The lowest number is {num1}.")
+        return num1
+
+
 number1 = float(input("Enter first number: "))
 number2 = float(input("Enter second number: "))
 number3 = float(input("Enter third number: "))
+
+ask_num(num1 = number1, num2 = number2, num3 = number3)
