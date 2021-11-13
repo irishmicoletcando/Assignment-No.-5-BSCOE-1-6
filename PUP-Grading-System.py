579ef8efa831fbd6f3df69504f23f02c82fdcbab
Irish Micole Cando <irishmicolecando@gmail.com>
2021-11-13 18:54:41 +0800

    Defined a function to ask the user their name and returned the function

---
+++ PUP-Grading-System.py
@@ -0,0 +1,12 @@
+# Program 1: PUP Grading System
+# Section 8 of https://www.pup.edu.ph/studentservices/files/ThePUPStudentHandbook2014.pdf
+# Create a program that will ask for grade percentage. Display the equivalent Grade/Mark and Description
+# Example:
+# Input grade: 87.6
+# Grade/Mark: 1.75
+# Description: Very Good
+
+def ask_user_name():
+    user_ = input("Enter name: ")
+    print(f"Welcome to PUP Grading System {user_}")
+    return user_
