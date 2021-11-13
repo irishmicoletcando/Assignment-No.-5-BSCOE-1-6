ccdede9eb33d3a61aaf1a8b42aff91ec368e86c2
Irish Micole Cando <irishmicolecando@gmail.com>
2021-11-13 22:54:58 +0800

    Added else then if condition for number 3

--- Lowest-Number.py
+++ Lowest-Number.py
@@ -11,6 +11,10 @@ def ask_num(num1, num2, num3):
         if num2 < num1 and num2 < num3:
             print(f"The lowest number is {num2}.")
             return num2
+        else:
+            if num3 < num1 and num3 < num1:
+                print(f"The lowest number is {num3}.")
+                return num3
 
 
 number1 = float(input("Enter first number: "))
