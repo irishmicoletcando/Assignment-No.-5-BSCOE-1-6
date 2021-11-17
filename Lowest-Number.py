1f8ee7afed23a03f88b53bdb400a76f39c345981
Irish Micole Cando <irishmicolecando@gmail.com>
2021-11-17 22:39:06 +0800

    Added else statement after try-except

--- Lowest-Number.py
+++ Lowest-Number.py
@@ -3,7 +3,7 @@
 # Find the lowest number using only if-else statement.
 # Display the lowest number.
 
-def ask_num(num1, num2, num3):
+def ask_num():
     try:
         num1 = float(input("Enter first number: "))
         num2 = float(input("Enter second number: "))
@@ -11,20 +11,21 @@ def ask_num(num1, num2, num3):
 
     except ValueError:
         print("Enter a number.")
-        
-    if num1 < num2 and num1 < num3:
-        print(f"The lowest number is {num1}.")
-        return num1
-    else: 
-        if num2 < num1 and num2 < num3:
-            print(f"The lowest number is {num2}.")
-            return num2
-        else:
-            if num3 < num1 and num3 < num1:
-                print(f"The lowest number is {num3}.")
-                return num3
-            else:
-                print("The three numbers are equal.")
-                return
 
-ask_num(num1 = number1, num2 = number2, num3 = number3)
+    else:
+        if num1 < num2 and num1 < num3:
+            print(f"The lowest number is {num1}.")
+            return num1
+        else: 
+            if num2 < num1 and num2 < num3:
+                print(f"The lowest number is {num2}.")
+                return num2
+            else:
+                if num3 < num1 and num3 < num1:
+                    print(f"The lowest number is {num3}.")
+                    return num3
+                else:
+                    print("The three numbers are equal.")
+                    return
+
+ask_num()
